540c923cc695661c9c1e86bccae2139ce5bf2685
laureano gonzalez <laureanomania@hotmail.com>
2024-07-11 12:23:16 -0300

    correccion preentrega 1

--- app.js
+++ app.js
@@ -1,54 +1,63 @@
-alert("Bienvenidos a nuestra Barberia");
-let nombre = prompt("Por favor, ingresa tu nombre:");
-let mensajeSaludo = "¡Hola, " + nombre + "! Bienvenido.";
-alert(mensajeSaludo);
-let edad = Number(prompt("Por favor, ingresa tu edad:"));
-edad = Number(edad);
-if (edad >= 18) {
-  alert("Eres mayor de edad.");
-} else {
-  alert("Eres menor de edad y necesitas la compañía de un adulto.");
+function solicitarNombre() {
+  return prompt("Bienvenido. Por favor, ingresa tu nombre.");
 }
 
-let precioBase = 7000;
+function mostrarPrecioBase() {
+  const precioBase = 7000;
+  alert(`El Valor de nuestro servicio es : $${precioBase}`);
+  return precioBase;
+}
 
-let precioTotal = precioBase;
+function seleccionarOpciones(precioBase) {
+  let precioTotal = precioBase;
+  let agregarMas = true;
 
-let seguirAgregando = true;
+  while (agregarMas) {
+    let opcion = confirm(
+      `Te interesaria agregar alguno de nuestros servicios adicionaleslaureano?\n\nPrecio total actual: $${precioTotal}`
+    );
 
-while (seguirAgregando) {
-  let opcion = prompt(
-    `Elige una opción adicional:\n\n1. Barba(+$1000)\n2. Cejas (+$650)\n3. Teñido (+$2000)\n4. Lavado (+$600)\n\nPara finalizar, escribe "fin".`
-  );
+    if (opcion) {
+      let opcionElegida = prompt(
+        `Por favor elige uno de nuestros Servicios adicionales:\n\n1. Barba (+$1000)\n2. Cejas (+$650)\n3. Teñido (+$2000)\n4. Lavado (+$600)\n\nPara finalizar, escribe "fin".`
+      );
 
-  if (opcion.toLowerCase() === "fin") {
-    seguirAgregando = false;
-  } else {
-    opcion = parseInt(opcion);
-
-    switch (opcion) {
-      case 1:
-        precioTotal += 1000;
-        alert("Has agregado Barba (+$1000).");
-        break;
-      case 2:
-        precioTotal += 650;
-        alert("Has agregado Cejas (+$650).");
-        break;
-      case 3:
-        precioTotal += 2000;
-        alert("Has agregado Teñido (+$2000).");
-        break;
-      case 4:
-        precioTotal += 600;
-        alert("Has agregado Lavado (+$600).");
-        break;
-      default:
-        alert("Opción no válida. Por favor, elige una opción válida.");
+      switch (opcionElegida) {
+        case "1":
+          precioTotal += 1000;
+          alert("Has agregado Barba (+$1000).");
+          break;
+        case "2":
+          precioTotal += 650;
+          alert("Has agregado Cejas (+$650).");
+          break;
+        case "3":
+          precioTotal += 2000;
+          alert("Has agregado Teñido (+$2000).");
+          break;
+        case "4":
+          precioTotal += 600;
+          alert("Has agregado Lavado (+$600).");
+          break;
+        case "fin":
+          agregarMas = false;
+          break;
+        default:
+          alert("Opción no válida. Por favor, elige una opción válida.");
+      }
+    } else {
+      agregarMas = false;
     }
   }
+
+  return precioTotal;
 }
-
-alert(`El precio total del servicio de uñas es: $${precioTotal}`);
-
-alert("Gracias por elegirnos");
+function ejecutarPrograma() {
+  let nombre = solicitarNombre();
+  alert(`¡Hola, ${nombre}! Bienvenido.`);
+  let precioBase = mostrarPrecioBase();
+  let precioFinal = seleccionarOpciones(precioBase);
+  alert(`El precio total a pagar es: $${precioFinal}`);
+  alert(`!Gracias por elejirnos , ${nombre}!`);
+}
+ejecutarPrograma();
